d7785dd522892225a933044e754050e66672f647
Vijaya Laxmi Kumbaji <Vijaya Laxmi Kumbaji@DESKTOP-NNQS1L8>
2026-02-05 12:14:24 -0700

    Created more sample test files for test scenarios

---
+++ DotNet.Testing/TestData/ExampleTests/HttpClientExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+
+namespace ApiTestHarness
+{
+    public static class HttpClientExtensions
+    {
+        private static readonly JsonSerializerOptions DefaultJson = new(JsonSerializerDefaults.Web);
+
+        public static async Task<T> GetJsonAsync<T>(this HttpClient client, string url, CancellationToken ct = default)
+        {
+            var resp = await client.GetAsync(url, ct);
+            resp.EnsureSuccessStatusCode();
+
+            var data = await resp.Content.ReadFromJsonAsync<T>(DefaultJson, ct);
+            return data ?? throw new InvalidOperationException("Response JSON was null.");
+        }
+
+        public static Task<HttpResponseMessage> PostJsonAsync<TBody>(this HttpClient client, string url, TBody body, CancellationToken ct = default)
+            => client.PostAsJsonAsync(url, body, DefaultJson, ct);
+
+        public static Task<HttpResponseMessage> PutJsonAsync<TBody>(this HttpClient client, string url, TBody body, CancellationToken ct = default)
+            => client.PutAsJsonAsync(url, body, DefaultJson, ct);
+    }
+
+}
---
+++ DotNet.Testing/TestData/ExampleTests/ResponseAssertions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace ApiTestHarness
+{
+    public static class ResponseAssertions
+    {
+        private static readonly JsonSerializerOptions DefaultJson = new(JsonSerializerDefaults.Web);
+
+        public static async Task<T> ShouldBeOkAndReadJsonAsync<T>(this HttpResponseMessage response)
+        {
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new InvalidOperationException($"Expected 200 OK but got {(int)response.StatusCode} {response.ReasonPhrase}");
+
+            var data = await response.Content.ReadFromJsonAsync<T>(DefaultJson);
+            return data ?? throw new InvalidOperationException("Response JSON was null.");
+        }
+
+        public static void ShouldHaveStatus(this HttpResponseMessage response, HttpStatusCode status)
+        {
+            if (response.StatusCode != status)
+                throw new InvalidOperationException($"Expected {(int)status} but got {(int)response.StatusCode} {response.ReasonPhrase}");
+        }
+    }
+}
+
